d2e7bf032ca236c68b2ec155e858761fff72434b
Kemunto Maragia <kmaragia@email.wm.edu>
2020-09-24 00:42:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,25 +1,21 @@
-def roll_call_dwarves(array)
-   hash = Hash.new
-   array.each_with_index { |item, index|  
-   puts"#{index+1}.#{item}"
- }
- hash   #=> {"cat"=>0, "dog"=>1, "wombat"=>2}
-  
+def roll_call_dwarves(dwarf_names)
+  dwarf_names.each_with_index{|item,index| puts "#{index +1}. #{item}"}
 end
 
-def summon_captain_planet (array)
-  x = array.collect do |c|
-    c.capitalize
+def summon_captain_planet (planeteer_calls)
+  planeteer_calls.collect {|x| x.capitalize+ "!"}
+end
+
+def long_planeteer_calls(words)
+  words.any?{|x| x.length > 4}
+end
+
+def find_the_cheese(ingredients)
+  cheese = ["cheddar","gouda","camembert"]
+  cheese.each do |x| 
+    if ingredients.include?(x)
+      return x 
+    end
   end
-  s = x.collect do |e|
-    e + "!"
-  end
-  return s
-end
-
-def long_planeteer_calls(array)
-  array.any? {|word| word.length> 4}
-end
-
-
+  return nil
 end
